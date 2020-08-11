562905540463296c9b777450c0d08e80627cce43
daphne tang <daphne.ch.tang@gmail.com>
2020-08-11 18:20:31 +0800

    enchance read.py

--- read.py
+++ read.py
@@ -1,4 +1,4 @@
-# 54/55 留言分析程式
+# 55 留言分析程式
 
 data = []
 count = 0 
@@ -10,8 +10,12 @@ with open('reviews.txt', 'r') as f:
         # %求餘數
         # count/100的餘數等於0，表示是1000的倍數，才印出來
             print(len(data))
-print(len(data))
+print('file read finished, total', len(data), 'records')
 
-print(data[0])
-print('---------------')
-print(data[1])
+# cound average length of 100000 reviews
+
+sum_len = 0
+for d in data:
+    sum_len = sum_len + len(d)
+print ('The average lengh of each review is', sum_len/len(data))
+
